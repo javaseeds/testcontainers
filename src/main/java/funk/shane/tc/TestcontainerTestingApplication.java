958cfa08e29bbaa99c332af71b2f6d716cfad4a8
Shane Funk <shanefunk@protonmail.com>
2020-05-01 21:25:57 -0500

    Initial setup

---
+++ src/main/java/funk/shane/tc/TestcontainerTestingApplication.java
@@ -0,0 +1,13 @@
+package funk.shane.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainerTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainerTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/funk/shane/tc/TestcontainerTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package funk.shane.tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainerTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
